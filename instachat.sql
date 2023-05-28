ad1507ea230a05f1e5ec314fed354887cdba9051
Francisco Javier Gañan Escobar <fgananescobar@gmail.com>
2023-05-28 15:08:28 +0200

    Cambios en el CSS
    Me falta agregar el CSS para los mensajes.

--- instachat.sql
+++ instachat.sql
@@ -1,76 +1,36 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2023 a las 08:25:29
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `instachat`
---
 CREATE DATABASE IF NOT EXISTS `instachat` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
 USE `instachat`;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `conversations`;
+CREATE TABLE `conversations` (
+  `id` int(11) NOT NULL,
+  `user1_id` int(11) NOT NULL,
+  `user2_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Estructura de tabla para la tabla `friends`
---
-
+DROP TABLE IF EXISTS `friends`;
 CREATE TABLE `friends` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `user_1` int(6) UNSIGNED DEFAULT NULL,
-  `user_2` int(6) UNSIGNED DEFAULT NULL
+  `id` int(11) NOT NULL,
+  `user_1` int(11) NOT NULL,
+  `user_2` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- RELACIONES PARA LA TABLA `friends`:
---   `user_1`
---       `users` -> `id`
---   `user_2`
---       `users` -> `id`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `posts`
---
-
-CREATE TABLE `posts` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `image_url` varchar(255) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `user_id` int(6) UNSIGNED DEFAULT NULL
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `conversation_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `sent_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- RELACIONES PARA LA TABLA `posts`:
---   `user_id`
---       `users` -> `id`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` int(6) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -78,85 +38,48 @@ CREATE TABLE `users` (
   `profile_picture` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- RELACIONES PARA LA TABLA `users`:
---
 
---
--- Volcado de datos para la tabla `users`
---
+ALTER TABLE `conversations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user1_id` (`user1_id`),
+  ADD KEY `user2_id` (`user2_id`);
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `cpassword`, `profile_picture`) VALUES
-(1, 'Maria', 'maria@gmail.com', 'contraseña', 'contraseña', '../resources/imgs/user-profiles/tumblr_p0k0bj766w1vy2tgqo7_500.png'),
-(2, 'Neku', 'neku@gmail.com', 'contraseña', 'contraseña', '../resources/imgs/user-profiles/prueba-icono.png'),
-(3, 'Roberto', 'mamapingas@gmail.com', 'a1a2a3', 'a1a2a3', '../resources/imgs/user-profiles/default-img.jpg');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `friends`
---
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_1` (`user_1`),
   ADD KEY `user_2` (`user_2`);
 
---
--- Indices de la tabla `posts`
---
-ALTER TABLE `posts`
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `conversation_id` (`conversation_id`),
+  ADD KEY `sender_id` (`sender_id`);
 
---
--- Indices de la tabla `users`
---
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `friends`
---
+ALTER TABLE `conversations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `friends`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `friends`
---
+ALTER TABLE `conversations`
+  ADD CONSTRAINT `conversations_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `conversations_ibfk_2` FOREIGN KEY (`user2_id`) REFERENCES `users` (`id`);
+
 ALTER TABLE `friends`
   ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_1`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`user_2`) REFERENCES `users` (`id`);
 
---
--- Filtros para la tabla `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
